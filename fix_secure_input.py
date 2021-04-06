ece86bd82ee8e7ed87f766cbe6868d7b3194f927
Chris Dzombak <chris@dzombak.com>
2021-04-06 14:18:11 -0400

    Make script compatible with python 2 _and_ 3
    previously this only worked if /usr/bin/python was python 3.

--- fix_secure_input.py
+++ fix_secure_input.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 import os
 import re
@@ -14,10 +14,10 @@ for ioreg_line in ioreg_out.splitlines():
     pids.add(pid)
 
 for pid in pids:
-    ps_out = os.popen(f"ps -p {pid} | sed -n '2 p'").read()
+    ps_out = os.popen("ps -p {} | sed -n '2 p'".format(pid)).read()
     ps_name = ps_out.split()[3]
     print('PID ' + pid + ' (' + ps_name + ') is using Secure Input')
     if 'loginwindow' in ps_name:
         os.popen("""osascript -e 'Tell application "System Events" to display dialog "loginwindow is using Secure Input. Lock & unlock the screen to fix it." with title "Secure Input" buttons {"OK"} default button 1 with icon caution'""").read()
     else:
-        os.popen(f"""osascript -e 'Tell application "System Events" to display dialog "{ps_name} (PID {pid}) is using Secure Input. Kill it?" with title "Secure Input" buttons {{"No", "Kill"}} default button 2 with icon caution' -e 'if result = {{button returned:"Kill"}} then' -e 'do shell script "kill {pid}"' -e 'end if'""").read()
+        os.popen("""osascript -e 'Tell application "System Events" to display dialog "{} (PID {}) is using Secure Input. Kill it?" with title "Secure Input" buttons {{"No", "Kill"}} default button 2 with icon caution' -e 'if result = {{button returned:"Kill"}} then' -e 'do shell script "kill {}"' -e 'end if'""".format(ps_name, pid, pid)).read()
